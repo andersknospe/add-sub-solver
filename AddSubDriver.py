6609a8f0e93fed0e57d9eaeeb32b8c875942510c
Anders Knospe <andersknospe@Anderss-MacBook-Pro.local>
2019-10-19 01:54:02 -0400

    renaming variables

--- AddSubDriver.py
+++ AddSubDriver.py
@@ -5,11 +5,11 @@
 '''
 from AddSubSolver import AddSubSolver
 
-aSS = AddSubSolver([1,3,4,9,3])
+A = AddSubSolver([1,3,4,9,3])
 target = 10 
-print(aSS)
-print("is 10 possible? : " + str(aSS.is_sum_possible(target)))
-print("path: " + aSS.get_path_to_sum(target)) + " = " + str(target)
-print("recursive: " + str(aSS.recursive_num_operations()))
-print("dp: " + str(aSS.dp_num_operations()))
+print(A)
+print("is 10 possible? : " + str(A.is_sum_possible(target)))
+print("path: " + A.get_path_to_sum(target)) + " = " + str(target)
+print("recursive: " + str(A.recursive_num_operations()))
+print("dp: " + str(A.dp_num_operations()))
 
